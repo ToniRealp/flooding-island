95663b0f25b4a7d0d5b7bb110b0e590f33c5f120
ToniRealp <tonirealp@gmail.com>
2022-05-11 00:19:57 +0200

    Added basic character controller

---
+++ Assets/scripts/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float speed = 5;
+    public float rotationSpeed = 2;
+    void Update()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    void MovePlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+    }
+
+    void RotatePlayer()
+    {
+        Vector3 targetDirection = target.position - transform.position;
+        transform.rotation= Quaternion.LookRotation(targetDirection, Vector3.up);
+    }
+}
