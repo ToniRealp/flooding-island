079b81b7f02fd3e8d9d62dbd80ec659548878078
ToniRealp <tonirealp@gmail.com>
2022-05-11 14:04:44 +0200

    Added sinking animation

--- Assets/scripts/CharacterController.cs
+++ Assets/scripts/CharacterController.cs
@@ -2,16 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     private new Rigidbody _rigidbody;
     public Transform target;
-    public float speed = 5;
+    public float targetVelocity = 10;
+
+    public float forceStrength = 500;
+    // public Vector3 targetSpeed = new Vector3(5,0,5);
   
     private Animator _animator;
     private static readonly int IsMoving = Animator.StringToHash("isMoving");
+
+    private bool onSolidGround = true;
 
 
     private void Start()
@@ -34,17 +40,34 @@ public class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if ((target.position - transform.position).magnitude > 5)
+        if ((target.position - transform.position).magnitude > 5 && onSolidGround)
         {
             MovePlayer();
         }
+
+        onSolidGround = false;
     }
 
     void MovePlayer()
     {
         var targetDirection = (target.position - transform.position).normalized;
         transform.rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
-        _rigidbody.MovePosition(transform.position + transform.forward * (speed * Time.fixedDeltaTime));
+        _rigidbody.MovePosition(transform.position + transform.forward * (targetVelocity * Time.fixedDeltaTime));
+    }
 
+    private void OnCollisionStay(Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.CompareTag("Ground"))
+        {
+            onSolidGround = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SinkTrigger"))
+        {
+            _animator.SetTrigger("isFalling");
+        }
     }
 }
