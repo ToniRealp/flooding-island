080234d3b61ae715f7cadfbf2e7e45c522704511
ToniRealp <tonirealp@gmail.com>
2022-05-11 02:03:23 +0200

    Added tile behaviour

--- Assets/scripts/CharacterController.cs
+++ Assets/scripts/CharacterController.cs
@@ -1,27 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private new Rigidbody _rigidbody;
     public Transform target;
     public float speed = 5;
-    public float rotationSpeed = 2;
+  
+    private Animator _animator;
+    private static readonly int IsMoving = Animator.StringToHash("isMoving");
+
+
+    private void Start()
+    {
+        _rigidbody = gameObject.GetComponent<Rigidbody>();
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
     void Update()
     {
-        MovePlayer();
-        RotatePlayer();
+        if ((target.position - transform.position).magnitude > 5)
+        {
+            _animator.SetBool(IsMoving, true);
+        }
+        else
+        {
+            _animator.SetBool(IsMoving, false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if ((target.position - transform.position).magnitude > 5)
+        {
+            MovePlayer();
+        }
     }
 
     void MovePlayer()
     {
-        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
-    }
+        var targetDirection = (target.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        _rigidbody.MovePosition(transform.position + transform.forward * (speed * Time.fixedDeltaTime));
 
-    void RotatePlayer()
-    {
-        Vector3 targetDirection = target.position - transform.position;
-        transform.rotation= Quaternion.LookRotation(targetDirection, Vector3.up);
     }
 }
---
+++ Assets/scripts/GroundTileController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTileController : MonoBehaviour
+{
+
+    public GameObject solid;
+    public GameObject island;
+    
+    public float timeToBreak = 3;
+    private bool _playerOnTop = false;
+
+    enum Stages
+    {
+        Solid, Island, Destroyed
+    }
+
+    private Stages _currentStage = Stages.Solid;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        solid = gameObject.transform.GetChild(1).gameObject;
+        island = gameObject.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerOnTop)
+        {
+            timeToBreak -= Time.deltaTime;
+            
+            if (timeToBreak <= 0)
+            {
+                switch (_currentStage)
+                {
+                    case Stages.Solid:
+                        _currentStage = Stages.Island;
+                        solid.SetActive(false);
+                        break;
+                    case Stages.Island:
+                        _currentStage = Stages.Destroyed;
+                        island.SetActive(false);
+                        break;
+                    case Stages.Destroyed:
+                        gameObject.GetComponent<Collider>().enabled = false;
+                        break;
+                }
+
+                timeToBreak = 3;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _playerOnTop = true;
+        }
+    }
+    
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _playerOnTop = false;
+        }
+    }
+}
