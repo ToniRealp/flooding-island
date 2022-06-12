0f5df8cd04b671f1a5d0de44561daa7461846f73
ToniRealp <tonirealp@gmail.com>
2022-06-12 14:51:40 +0200

    Integrated menu ingame

--- Assets/scripts/FloodingIslandSceneController.cs
+++ Assets/scripts/FloodingIslandSceneController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,6 +9,11 @@ public class FloodingIslandSceneController : MonoBehaviour
 {
     public GameStartColliderController player1ColliderController;
     public GameStartColliderController player2ColliderController;
+    public GameObject mainTitle;
+    public GameObject countdownTextGameObject;
+
+    private TextMeshProUGUI countdownText;
+    
 
     public float timeToStart = 3;
     private void OnEnable()
@@ -20,6 +26,11 @@ public class FloodingIslandSceneController : MonoBehaviour
         EventManager.OnGameOver.RemoveListener(ResetGame);
     }
 
+    private void Start()
+    {
+        countdownText = countdownTextGameObject.GetComponent<TextMeshProUGUI>();
+    }
+
     private void Update()
     {
         if (player1ColliderController.playerReady && player2ColliderController.playerReady && timeToStart > 0)
@@ -30,8 +41,12 @@ public class FloodingIslandSceneController : MonoBehaviour
         if (timeToStart <= 0)
         {
             EventManager.OnGameStart.Invoke();
+            mainTitle.SetActive(false);
+            countdownTextGameObject.SetActive(false);
         }
-        
+
+        countdownText.text = $"{timeToStart:0.0}";
+
     }
 
     void ResetGame(EventManager.OnGameOverInfo gameOverInfo)
